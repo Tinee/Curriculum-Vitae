c68e2e630880162b62136a70ab5d5f35e26be54a
Marcus Carlsson <Marcus Carlsson>
2016-04-03 20:18:55 +0200

    Fixed the UserHandler.

--- Server/Business Logic/Database/UserHandler.cs
+++ Server/Business Logic/Database/UserHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace Business_Logic.Database
+﻿using System.Collections.Generic;
+using Contracts;
+using DataLayer.Logic.Database.UnitOfWork;
+using Mappers;
+
+namespace Business_Logic.Database
 {
     public class UserHandler
     {
+        private UnitOfWork _uow;
+
+        public List<User> Get()
+        {
+            return _uow.UserRepository.Get().ToContracts();
+        }
+
+        public User Get(int id)
+        {
+            return _uow.UserRepository.Get(id).ToContract();
+        }
+
+        public void Post(User user)
+        {
+            _uow.UserRepository.CreateOrUpdate(user.ToDatabaseEntitie());
+        }
+
+        public void Delete(int id)
+        {
+            _uow.UserRepository.Delete(id);
+        }
     }
 }
