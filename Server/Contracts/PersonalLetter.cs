9f3b837d05803b0bdaa0bfb3f147704265c65517
Marcus Carlsson <Marcus Carlsson>
2016-04-03 20:17:10 +0200

    Added contracts libary with some contracts.

---
+++ Server/Contracts/PersonalLetter.cs
@@ -0,0 +1,15 @@
+﻿using Enums;
+
+namespace Contracts
+{
+    public class PersonalLetter
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public Company Company { get; set; }
+        public User ContactPerson { get; set; }
+        public int LoginCount { get; set; }
+        public bool Active { get; set; }
+        public Permission Permission { get; set; }
+    }
+}
--- Server/Contracts/User.cs
+++ Server/Contracts/User.cs
@@ -2,6 +2,11 @@
 {
     public class User
     {
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+
+        public  Type { get; set; }
     }
 }
