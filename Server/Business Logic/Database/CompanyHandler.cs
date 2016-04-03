217993d6af5476d7e08d82dc1209f621b8863d38
Marcus Carlsson <Marcus Carlsson>
2016-04-03 20:19:27 +0200

    Added a CompanyHandler.

---
+++ Server/Business Logic/Database/CompanyHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Contracts;
+using DataLayer.Logic.Database.UnitOfWork;
+using Mappers;
+
+namespace Business_Logic.Database
+{
+    public class CompanyHandler
+    {
+        private UnitOfWork _uow;
+
+        public List<Company> Get()
+        {
+            return _uow.CompanyRepository.Get().ToContracts();
+        }
+
+        public User Get(int id)
+        {
+            return _uow.UserRepository.Get(id).ToContract();
+        }
+
+        public void Post(User user)
+        {
+            _uow.UserRepository.CreateOrUpdate(user.ToDatabaseEntitie());
+        }
+
+        public void Delete(int id)
+        {
+            _uow.UserRepository.Delete(id);
+        }
+    }
+}
