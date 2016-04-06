4ce45c5ba52ba1f2ea786ab4dae1bea35c5e2a6b
Marcus Carlsson <Marcus Carlsson>
2016-04-06 22:16:21 +0200

    Some logic to fetch and display data on view.

--- Client/app/layout/layoutController.js
+++ Client/app/layout/layoutController.js
@@ -1,26 +1,57 @@
 (function() {
-'use strict';
+   'use strict';
 
    angular
       .module('layout.module')
       .controller('LayoutController', LayoutController);
 
-   LayoutController.$inject = ['levelbar','dataservice'];
-   function LayoutController(levelbar,dataservice) {
+   LayoutController.$inject = ['levelbar', 'dataservice','toastr'];
+   function LayoutController(levelbar, dataservice,toastr) {
       var vm = this;
-      
+
 
       activate();
+
+      vm.personalLetter = null;
+      vm.foundLetter = false;
+      vm.companyKey = "";
+
+      vm.getPersonalLetter = getPersonalLetter;
+      vm.openModal = openModal;
 
       ////////////////
 
       function activate() {
          levelbar.getValues();
-         dataservice.personalLetters().get({companyKey:"itm"},function(response){
-            var x = response;
-         },function(err){
-            alert('Error');
+         openModal();
+      }
+
+      function openModal() {
+         $("#myModal").modal();
+      }
+
+      function getPersonalLetter(companyKey) {
+         dataservice.personalLetters().get({ companyKey: companyKey }, function(response) {
+            
+            vm.personalLetter = response;
+            vm.foundLetter = true;
+            
+            $('#myModal').modal('hide');
+            
+             toastr.success('Jag hittade erat personliga brev.', 'Välkommen ' + vm.personalLetter.Company.Name, {
+                    timeOut: 6000,
+                    closeButton: true
+                });
+
+         }, function(err) {
+            
+             toastr.error('Kunde inte hitta det personliga brevet.', 'Fel', {
+                    timeOut: 6000,
+                    closeButton: true
+                });
+            
          });
-       }
+      }
    }
-})();
+}
+)();
