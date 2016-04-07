e3117671de5be4abc6ce6a3030dd3352be36b49b
Marcus Carlsson <Marcus Carlsson>
2016-04-07 21:52:17 +0200

    Some logic for localstorage.

--- Client/app/layout/layoutController.js
+++ Client/app/layout/layoutController.js
@@ -5,51 +5,76 @@
       .module('layout.module')
       .controller('LayoutController', LayoutController);
 
-   LayoutController.$inject = ['levelbar', 'dataservice','toastr'];
-   function LayoutController(levelbar, dataservice,toastr) {
+   LayoutController.$inject = ['levelbar', 'dataservice', 'toastr', 'localStorageService'];
+   function LayoutController(levelbar, dataservice, toastr, localStorageService) {
       var vm = this;
 
-
-      activate();
-
-      vm.personalLetter = null;
+      var localStoragePersonalLetter =
+         vm.personalLetter = null;
       vm.foundLetter = false;
       vm.companyKey = "";
 
       vm.getPersonalLetter = getPersonalLetter;
       vm.openModal = openModal;
+      vm.removeLocalStorageItem = removeLocalStorageItem;
+
+      activate();
 
       ////////////////
 
       function activate() {
          levelbar.getValues();
-         openModal();
+         var personalLetter = getLocalStorage('personalLetter')
+         if (personalLetter === null) {
+            $("#myModal").modal();
+
+         }
+         {
+            vm.foundLetter = true;
+            vm.personalLetter = personalLetter;
+         }
       }
 
       function openModal() {
-         $("#myModal").modal();
+         var personalLetter = getLocalStorage('personalLetter')
+
+         if (personalLetter === null) {
+            $("#myModal").modal();
+         }
+         else {
+            vm.foundLetter = true;
+         }
+      }
+
+      function removeLocalStorageItem(key) {
+         localStorageService.remove(key);
+         vm.foundLetter = false;
+      }
+
+      function getLocalStorage(key) {
+         return localStorageService.get(key);
       }
 
       function getPersonalLetter(companyKey) {
+
          dataservice.personalLetters().get({ companyKey: companyKey }, function(response) {
-            
+
             vm.personalLetter = response;
             vm.foundLetter = true;
-            
+            localStorageService.set('personalLetter', vm.personalLetter);
+
             $('#myModal').modal('hide');
-            
-             toastr.success('Erat personliga brev ligger nu på hemsidan.', 'Hej ' + vm.personalLetter.Company.Name, {
-                    timeOut: 6000,
-                    closeButton: true
-                });
+            toastr.success('Erat personliga brev ligger nu på hemsidan.', 'Hej ' + vm.personalLetter.Company.Name, {
+               timeOut: 6000,
+               closeButton: true
+            });
 
          }, function(err) {
-            
-             toastr.error('Kunde inte hitta det personliga brevet.', 'Fel', {
-                    timeOut: 6000,
-                    closeButton: true
-                });
-            
+
+            toastr.error('Kunde inte hitta det personliga brevet.', 'Fel', {
+               timeOut: 6000,
+               closeButton: true
+            });
          });
       }
    }
