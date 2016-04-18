6591b977d3eec0aa5feeb50a71b5b3d31a9266de
Marcus Carlsson <Marcus Carlsson>
2016-04-19 01:20:17 +0200

    Fixed account files.

---
+++ Admin Mobile Application/www/account/account.js
@@ -0,0 +1,26 @@
+(function() {
+'use strict';
+
+   angular
+      .module('app.account')
+      .controller('AccountController', AccountController);
+
+   AccountController.$inject = ['$state'];
+   function AccountController($state) {
+      var vm = this;
+      
+
+      activate();
+      
+      vm.logout = logout;
+
+      ////////////////
+
+      function activate() { }
+      
+       function logout() {
+            window.localStorage.removeItem('token');
+            $state.go('tab.login');
+        }
+   }
+})();
