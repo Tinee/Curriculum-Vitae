584ff727f856dde3b3b3dc936e82ab8d5313b394
Marcus Carlsson <Marcus Carlsson>
2016-04-19 01:21:56 +0200

    Added so the httpInterceptor works correctly.

--- Admin Mobile Application/www/core/config.js
+++ Admin Mobile Application/www/core/config.js
@@ -1,1 +1,14 @@
-// Init my in page when needed.
+(function() {
+    'use strict';
+
+    angular.module('app.core').config(httpInterceptor);
+
+    ///////////////////////////////////////////////////
+
+    httpInterceptor.$inject = ['$httpProvider'];
+    function httpInterceptor($httpProvider) {
+        $httpProvider.interceptors.push('authInterceptorService');
+        $httpProvider.defaults.headers.post = { 'Content-Type': 'application/json' };
+    }
+
+})();
