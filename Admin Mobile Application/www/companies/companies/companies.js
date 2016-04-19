9523007cb60f38148ae9b529b0b81f3c2aef1b23
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-04-19 15:27:15 +0200

    Added a modal to allow access to newCompany.html.

--- Admin Mobile Application/www/companies/companies/companies.js
+++ Admin Mobile Application/www/companies/companies/companies.js
@@ -1,48 +1,66 @@
-(function () {
-   'use strict';
+(function() {
+    'use strict';
 
-   angular
-      .module('app.companies')
-      .controller('CompaniesController', CompaniesController);
+    angular
+        .module('app.companies')
+        .controller('CompaniesController', CompaniesController);
 
-   CompaniesController.$inject = ['dataservice', '$scope'];
-   function CompaniesController(dataservice, $scope) {
-      var vm = this;
+    CompaniesController.$inject = ['dataservice', '$scope', '$ionicModal'];
+    function CompaniesController(dataservice, $scope, $ionicModal) {
+        var vm = this;
 
-      vm.personalLetters = [];
-      vm.doRefresh = doRefresh;
+        vm.personalLetters = [];
+        vm.doRefresh = doRefresh;
+        vm.triggerModal = triggerModal;
+        $scope.closeModal = closeModal;
 
-      activate(
+        activate();
 
-      );
-      ////////////////
 
-      function activate() {
+        ////////////////
 
-         var storageItem = JSON.parse(window.localStorage.getItem('personalLetters'));
+        function activate() {
 
-         if (storageItem) {
-            vm.personalLetters = storageItem.personalLetters;
-         }
-         else {
-            dataservice.personalLetters().query(function (response) {
-               window.localStorage.setItem('personalLetters', angular.toJson({ personalLetters: response }));
-               vm.personalLetters = response;
+            var storageItem = JSON.parse(window.localStorage.getItem('personalLetters'));
+
+            if (storageItem) {
+                vm.personalLetters = storageItem.personalLetters;
+            }
+            else {
+                dataservice.personalLetters().query(function(response) {
+                    window.localStorage.setItem('personalLetters', angular.toJson({ personalLetters: response }));
+                    vm.personalLetters = response;
+                });
+            }
+        }
+
+        function triggerModal() {
+
+            $ionicModal.fromTemplateUrl('./companies/companies/addCompany.html', {
+                scope: $scope,
+                animation: 'slide-in-up',
+                hardwareBackButtonClose: false
+            }).then(function(modal) {
+                $scope.modal = modal;
+                $scope.modal.show();
             });
-         }
-      }
+        };
 
-      function doRefresh() {
-         var newData = dataservice.personalLetters().query(function (response) {
-            
-            window.localStorage.removeItem('personalLetters');
-            window.localStorage.setItem('personalLetters', angular.toJson({ personalLetters: response }));
-            vm.personalLetters = response;
-            $scope.$broadcast('scroll.refreshComplete');
-            
-         });
-      }
+        function closeModal() {
+            $scope.modal.hide();
+        };
 
-   }
+        function doRefresh() {
+            var newData = dataservice.personalLetters().query(function(response) {
+
+                window.localStorage.removeItem('personalLetters');
+                window.localStorage.setItem('personalLetters', angular.toJson({ personalLetters: response }));
+                vm.personalLetters = response;
+                $scope.$broadcast('scroll.refreshComplete');
+
+            });
+        }
+
+    }
 })();
 
