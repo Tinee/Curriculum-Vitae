7143ea7cd201da77ba2dfe156a238dce526234fe
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-04-22 15:56:19 +0200

    Added letter html,controller and module.

---
+++ Admin Mobile Application/www/letter/letter.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+    angular
+        .module('app.letter')
+        .controller('LetterController', LetterController);
+
+    LetterController.$inject = [];
+    function LetterController() {
+        var vm = this;
+
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
