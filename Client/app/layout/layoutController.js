88f545df58205dd671da93e458784bb1a05a8bb7
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-04-26 14:18:19 +0200

    Fixed so the section for my skills loads from database.

--- Client/app/layout/layoutController.js
+++ Client/app/layout/layoutController.js
@@ -1,81 +1,92 @@
-(function() {
-   'use strict';
+(function () {
+    'use strict';
 
-   angular
-      .module('layout.module')
-      .controller('LayoutController', LayoutController);
+    angular
+        .module('layout.module')
+        .controller('LayoutController', LayoutController);
 
-   LayoutController.$inject = ['levelbar', 'dataservice', 'toastr', 'localStorageService'];
-   function LayoutController(levelbar, dataservice, toastr, localStorageService) {
-      var vm = this;
+    LayoutController.$inject = ['levelbar', 'dataservice', 'toastr', 'localStorageService', '$scope'];
+    function LayoutController(levelbar, dataservice, toastr, localStorageService, $scope) {
+        var vm = this;
 
-      vm.personalLetter = null;
-      vm.foundLetter = false;
-      vm.companyKey = "";
+        vm.personalLetter = null;
+        vm.foundLetter = false;
+        vm.companyKey = "";
 
-      vm.getPersonalLetter = getPersonalLetter;
-      vm.openModal = openModal;
-      vm.removeLocalStorageItem = removeLocalStorageItem;
+        vm.getPersonalLetter = getPersonalLetter;
+        vm.openModal = openModal;
+        vm.removeLocalStorageItem = removeLocalStorageItem;
 
-      activate();
+        activate();
 
-      ////////////////
+        ////////////////
 
-      function activate() {
-         levelbar.getValues();
-         var personalLetter = getLocalStorage('personalLetter')
-         if (personalLetter === null) {
-            $("#myModal").modal();
+        $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+            levelbar.getValues();
+        });
 
-         }
-         else {
-            vm.foundLetter = true;
-            vm.personalLetter = personalLetter;
-         }
-      }
+        function activate() {
+            var personalLetter = getLocalStorage('personalLetter')
+            if (personalLetter === null) {
+                $("#myModal").modal();
 
-      function openModal() {
-         var personalLetter = getLocalStorage('personalLetter')
+            }
+            else {
+                vm.foundLetter = true;
+                vm.personalLetter = personalLetter;
+            }
 
-         if (personalLetter === null) {
-            $("#myModal").modal();
-         }
-         else {
-            vm.foundLetter = true;
-         }
-      }
+            getTechnicians()
+        }
 
-      function removeLocalStorageItem(key) {
-         localStorageService.remove(key);
-         vm.foundLetter = false;
-      }
+        function openModal() {
+            var personalLetter = getLocalStorage('personalLetter')
 
-      function getLocalStorage(key) {
-         return localStorageService.get(key);
-      }
+            if (personalLetter === null) {
+                $("#myModal").modal();
+            }
+            else {
+                vm.foundLetter = true;
+            }
+        }
 
-      function getPersonalLetter(companyKey) {
+        function removeLocalStorageItem(key) {
+            localStorageService.remove(key);
+            vm.foundLetter = false;
+        }
 
-         dataservice.personalLetters().get({ companyKey: companyKey }, function(response) {
+        function getLocalStorage(key) {
+            return localStorageService.get(key);
+        }
 
-            vm.personalLetter = response;
-            vm.foundLetter = true;
-            localStorageService.set('personalLetter', vm.personalLetter);
+        function getTechnicians() {
+            vm.technicians = dataservice.technicians().query(function (response) {
 
-            $('#myModal').modal('hide');
-            toastr.success('Erat personliga brev ligger nu på hemsidan.', 'Hej ' + vm.personalLetter.Company.Name, {
-               timeOut: 6000,
-               closeButton: false
             });
+        }
 
-         }, function(err) {
+        function getPersonalLetter(companyKey) {
 
-            toastr.error('Kunde inte hitta det personliga brevet.', 'Fel', {
-               timeOut: 6000,
-               closeButton: true
+            dataservice.personalLetters().get({ companyKey: companyKey }, function (response) {
+
+                vm.personalLetter = response;
+                vm.foundLetter = true;
+                localStorageService.set('personalLetter', vm.personalLetter);
+
+                $('#myModal').modal('hide');
+                toastr.success('Erat personliga brev ligger nu på hemsidan.', 'Hej ' + vm.personalLetter.Company.Name, {
+                    timeOut: 6000,
+                    closeButton: false
+                });
+
+            }, function (err) {
+
+                toastr.error('Kunde inte hitta det personliga brevet.', 'Fel', {
+                    timeOut: 6000,
+                    closeButton: true
+                });
             });
-         });
-      }
-   }
+        }
+    }
 }
 )();
